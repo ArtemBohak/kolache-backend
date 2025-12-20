d3a9f56544d88840367bdbe1f41bb1db707b6d85
Artem Bohak <artem.bohak@hotline.finance>
2025-12-20 22:30:41 +0200

    temp

--- src/microservices/kolache-bot/controllers/poster-webhooks.controller.ts
+++ src/microservices/kolache-bot/controllers/poster-webhooks.controller.ts
@@ -7,12 +7,21 @@ export class PosterWebhooksController {
 
   @Post()
   @HttpCode(HttpStatus.OK)
-  async handleWebhooks(@Body() body: { object: string; action: string }) {
+  async handleWebhooks(
+    @Body() body: { object: string; action: string; data: any },
+  ) {
     console.log('Received webhook:', body);
     if (body.object === 'incoming_order' && body.action === 'added') {
       await this.telegramApiService.sendMessageToKolacheChannel(
         'Увага, нове онлайн замовлення, перевірте планшет',
       );
+    }
+
+    if (body.object === 'transaction' && body.action === 'changed') {
+      await this.telegramApiService.sendTextMessageToUser({
+        message: `Зміни в чеку на суму ${body?.data?.transactions_history?.value_text}`,
+        chatId: 532890534,
+      });
     }
     return;
   }
